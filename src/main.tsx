3cf7ccc94f158255b4c616ccc46196e129d35d8f
umiadore <umi.adore@gmail.com>
2025-02-12 01:16:29 +0400

    1

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,11 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, HashRouter } from 'react-router-dom'
 
 
 createRoot(document.getElementById('root')!).render(
-  <BrowserRouter basename='/frontend-ts-consultations'>
+  <HashRouter>
   <App />
-  </BrowserRouter>
+  </HashRouter>
 )
